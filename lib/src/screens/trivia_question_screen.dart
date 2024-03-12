43424affca499f6eeb1987ff188ae91530e500b5
ah682 <96323387+ah682@users.noreply.github.com>
2024-03-12 22:30:23 +0000

    Implement diverse question types
    Introduced multiple question types to the trivia game, enhancing its depth and variety. The question types now include:
    
    - Year of release: Players guess the release year of the movie.
    - Genre: Players identify the genre(s) the movie belongs to.
    - Director: Players guess who directed the movie.
    
    Each round randomly selects one of these question types, and generates corresponding answer options based on the movie's data fetched from the TMDB API. This addition aims to provide a more comprehensive and engaging experience for the players, testing their knowledge across different aspects of the movies.

--- lib/src/screens/trivia_question_screen.dart
+++ lib/src/screens/trivia_question_screen.dart
@@ -2,26 +2,46 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 import 'dart:math';
-import 'answer.dart'; // Make sure this import points to your correct file
-import 'game_over.dart'; // Make sure this import points to your correct file
+import 'answer.dart';
+import 'game_over.dart';
+
+enum QuestionType { Year, Genre, Director }
 
 class Movie {
   final String title;
   final int releaseYear;
   final String imageUrl;
+  final List<String> genres;
+  final String director;
 
   Movie({
     required this.title,
     required this.releaseYear,
     required this.imageUrl,
+    required this.genres,
+    required this.director,
   });
 
-  // Dynamically generate movie image for each question
   factory Movie.fromJson(Map<String, dynamic> json) {
+    List<dynamic> crew = json['credits']?['crew'] ?? [];
+    String directorName = "Unknown";
+    if (crew.isNotEmpty) {
+      final director = crew.firstWhere(
+        (member) => member['job'] == 'Director',
+        orElse: () => null,
+      );
+      if (director != null) {
+        directorName = director['name'];
+      }
+    }
+
     return Movie(
       title: json['title'] as String,
       releaseYear: DateTime.parse(json['release_date'] as String).year,
       imageUrl: 'https://image.tmdb.org/t/p/w500${json['poster_path']}',
+      genres: List<String>.from(json['genre_ids'].map((id) =>
+          id.toString())), // Replace with actual genre names based on IDs
+      director: directorName,
     );
   }
 }
@@ -31,7 +51,8 @@ class MovieApi {
   final String apiKey = 'cb250fc48cd4dc439eae0be94d1d7bd1';
 
   Future<List<Movie>> fetchPopularMovies() async {
-    final url = Uri.parse('$baseUrl/movie/popular?api_key=$apiKey');
+    final url = Uri.parse(
+        '$baseUrl/movie/popular?api_key=$apiKey&append_to_response=credits');
     final response = await http.get(url);
 
     if (response.statusCode == 200) {
@@ -53,8 +74,9 @@ class _TriviaGameState extends State<TriviaGame> {
   int currentRound = 1;
   int score = 0;
   late Movie currentMovie;
-  List<int> answerOptions = [];
+  List<dynamic> answerOptions = [];
   bool isLoading = true;
+  QuestionType? questionType;
 
   @override
   void initState() {
@@ -63,27 +85,73 @@ class _TriviaGameState extends State<TriviaGame> {
   }
 
   fetchAndSetupRound() async {
+    setState(() => isLoading = true);
     MovieApi api = MovieApi();
     List<Movie> movies = await api.fetchPopularMovies();
     Random random = Random();
     currentMovie = movies[random.nextInt(movies.length)];
 
-    answerOptions = List.generate(
-        4, (index) => currentMovie.releaseYear - random.nextInt(10) + 5);
-    answerOptions[random.nextInt(answerOptions.length)] =
-        currentMovie.releaseYear;
+    List<QuestionType> questionTypes = QuestionType.values;
+    questionType = questionTypes[random.nextInt(questionTypes.length)];
 
-    setState(() {
-      isLoading = false;
-    });
-  }
-
-  void handleAnswer(int selectedYear) {
-    bool isCorrect = selectedYear == currentMovie.releaseYear;
-    if (isCorrect) {
-      score++;
+    switch (questionType) {
+      case QuestionType.Year:
+        answerOptions = List.generate(
+            4, (_) => currentMovie.releaseYear - random.nextInt(10) + 5);
+        answerOptions[random.nextInt(4)] = currentMovie.releaseYear;
+        break;
+      case QuestionType.Genre:
+        List<String> allGenres = [
+          'Action',
+          'Drama',
+          'Comedy',
+          'Thriller'
+        ]; // Example genres
+        answerOptions = currentMovie.genres..shuffle();
+        while (answerOptions.length < 4) {
+          String randomGenre = allGenres[random.nextInt(allGenres.length)];
+          if (!answerOptions.contains(randomGenre))
+            answerOptions.add(randomGenre);
+        }
+        answerOptions.shuffle();
+        break;
+      case QuestionType.Director:
+        answerOptions = [
+          'John Doe',
+          'Jane Smith',
+          'Michael Bay',
+          currentMovie.director
+        ]; // Example directors
+        answerOptions.shuffle();
+        break;
+      default:
+        break;
     }
 
+    setState(() => isLoading = false);
+  }
+
+  void handleAnswer(dynamic selectedAnswer) {
+    bool isCorrect = false;
+    switch (questionType) {
+      case QuestionType.Year:
+        isCorrect = selectedAnswer == currentMovie.releaseYear;
+        break;
+      case QuestionType.Genre:
+        isCorrect = currentMovie.genres.contains(selectedAnswer);
+        break;
+      case QuestionType.Director:
+        isCorrect = selectedAnswer == currentMovie.director;
+        break;
+      default:
+        break;
+    }
+
+    if (isCorrect) score++;
+    navigateToNextScreen(isCorrect);
+  }
+
+  navigateToNextScreen(bool isCorrect) {
     if (currentRound < 5) {
       Navigator.push(
         context,
@@ -93,49 +161,33 @@ class _TriviaGameState extends State<TriviaGame> {
             currentScore: score,
             currentRound: currentRound,
             onNextRound: () {
-              Navigator.of(context)
-                  .pop(); // Pop the AnswerScreen off the navigation stack
+              Navigator.of(context).pop();
               setState(() {
                 currentRound++;
-                isLoading = true;
+                fetchAndSetupRound();
               });
-              fetchAndSetupRound();
             },
           ),
         ),
       );
     } else {
       Navigator.pushReplacement(
-        context,
-        MaterialPageRoute(
-          builder: (context) => GameOverScreen(score: score),
-        ),
-      );
+          context,
+          MaterialPageRoute(
+              builder: (context) => GameOverScreen(score: score)));
     }
-  }
-
-  void resetGame() {
-    setState(() {
-      currentRound = 1;
-      score = 0;
-      isLoading = true;
-    });
-    fetchAndSetupRound();
   }
 
   @override
   Widget build(BuildContext context) {
     if (isLoading) {
       return Scaffold(
-        appBar: AppBar(title: Text('Movie Trivia')),
-        body: Center(child: CircularProgressIndicator()),
-      );
+          appBar: AppBar(title: Text('Movie Trivia')),
+          body: Center(child: CircularProgressIndicator()));
     }
 
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Movie Trivia'),
-      ),
+      appBar: AppBar(title: Text('Movie Trivia')),
       body: Center(
         child: SingleChildScrollView(
           child: Column(
@@ -143,45 +195,49 @@ class _TriviaGameState extends State<TriviaGame> {
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
               SizedBox(height: 20),
-              Image.network(
-                currentMovie
-                    .imageUrl, // Use the imageUrl from the currentMovie object
-                height: 150,
-                width: double.infinity,
-                fit: BoxFit.cover,
-                errorBuilder: (context, error, stackTrace) {
-                  return Icon(Icons
-                      .error); // Display an error icon if the image fails to load
-                },
-              ),
+              Image.network(currentMovie.imageUrl,
+                  height: 150,
+                  width: double.infinity,
+                  fit: BoxFit.cover,
+                  errorBuilder: (context, error, stackTrace) =>
+                      Icon(Icons.error)),
               SizedBox(height: 20),
               Divider(thickness: 2),
               Text(
-                'Round $currentRound: What year was "${currentMovie.title}" released?',
+                getQuestionText(),
                 style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
                 textAlign: TextAlign.center,
               ),
               SizedBox(height: 20),
-              ...answerOptions
-                  .map((year) => Padding(
-                        padding: const EdgeInsets.symmetric(vertical: 8.0),
-                        child: ElevatedButton(
-                          onPressed: () => handleAnswer(year),
-                          child: Text(year.toString()),
-                          style: ElevatedButton.styleFrom(
-                            padding: EdgeInsets.symmetric(
-                                horizontal: 30, vertical: 15),
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(30),
-                            ),
-                          ),
-                        ),
-                      ))
-                  .toList(),
+              ...answerOptions.map((option) => Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 8.0),
+                    child: ElevatedButton(
+                      onPressed: () => handleAnswer(option),
+                      child: Text(option.toString()),
+                      style: ElevatedButton.styleFrom(
+                          padding: EdgeInsets.symmetric(
+                              horizontal: 30, vertical: 15),
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(30))),
+                    ),
+                  )),
             ],
           ),
         ),
       ),
     );
+  }
+
+  String getQuestionText() {
+    switch (questionType) {
+      case QuestionType.Year:
+        return 'What year was "${currentMovie.title}" released?';
+      case QuestionType.Genre:
+        return 'Which genre does "${currentMovie.title}" belong to?';
+      case QuestionType.Director:
+        return 'Who directed "${currentMovie.title}"?';
+      default:
+        return '';
+    }
   }
 }
