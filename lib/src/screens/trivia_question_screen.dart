6f3060805a5797ca379cb4af2a2ab7adef5259a1
ah682 <96323387+ah682@users.noreply.github.com>
2024-03-14 06:19:04 +0000

    Implement package connectivity
    Implement package connectivity so if no connectivity game is saved

--- lib/src/screens/trivia_question_screen.dart
+++ lib/src/screens/trivia_question_screen.dart
@@ -2,23 +2,21 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 import 'dart:math';
-import 'answer.dart'; // Screen to display after answering a question
-import 'game_over.dart'; // Screen to display when the game is over
+import 'answer.dart';
+import 'game_over.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'intro_screen.dart'; // Ensure this is the correct import for your IntroScreen
+import 'package:connectivity/connectivity.dart';
 
-// Enum representing the type of question that can be asked.
 enum QuestionType { Year, Genre, Director }
 
-// Class to represent a movie with its associated data.
 class Movie {
-  final String title; // Movie title
-  final int releaseYear; // Year the movie was released
-  final String imageUrl; // URL for the movie's image
-  final List<String> genres; // List of genres the movie belongs to
-  final String director; // Director of the movie
+  final String title;
+  final int releaseYear;
+  final String imageUrl;
+  final List<String> genres;
+  final String director;
 
-  // Constructor for initializing the Movie object.
   Movie({
     required this.title,
     required this.releaseYear,
@@ -27,11 +25,9 @@ class Movie {
     required this.director,
   });
 
-  // Factory constructor to create a Movie object from JSON data.
   factory Movie.fromJson(Map<String, dynamic> json) {
     List<dynamic> crew = json['credits']?['crew'] ?? [];
     String directorName = "Unknown";
-    // Find the first crew member who is the director and get their name.
     if (crew.isNotEmpty) {
       final director = crew.firstWhere(
         (member) => member['job'] == 'Director',
@@ -42,7 +38,6 @@ class Movie {
       }
     }
 
-    // Return a new Movie object populated with data from the JSON.
     return Movie(
       title: json['title'] as String,
       releaseYear: DateTime.parse(json['release_date'] as String).year,
@@ -53,22 +48,18 @@ class Movie {
   }
 }
 
-// API class to fetch movies from an online database.
 class MovieApi {
   final String baseUrl = 'https://api.themoviedb.org/3/';
-  final String apiKey = 'your_api_key_here'; // Place your TMDB API Key here
+  final String apiKey = 'cb250fc48cd4dc439eae0be94d1d7bd1';
 
-  // Fetches popular movies and returns a list of Movie objects.
   Future<List<Movie>> fetchPopularMovies() async {
     final url = Uri.parse(
         '$baseUrl/movie/popular?api_key=$apiKey&append_to_response=credits');
     final response = await http.get(url);
 
-    // Checks if the HTTP request was successful.
     if (response.statusCode == 200) {
       final data = json.decode(response.body);
       final List<dynamic> moviesJson = data['results'];
-      // Maps through the JSON data and converts each movie to a Movie object.
       return moviesJson.map((json) => Movie.fromJson(json)).toList();
     } else {
       throw Exception('Failed to load popular movies');
@@ -76,56 +67,62 @@ class MovieApi {
   }
 }
 
-// StatefulWidget for the main game logic and display.
 class TriviaGame extends StatefulWidget {
   @override
   _TriviaGameState createState() => _TriviaGameState();
 }
 
-// State class for TriviaGame which holds the state of the game.
 class _TriviaGameState extends State<TriviaGame> {
-  int currentRound = 1; // Current round number
-  int score = 0; // Current score
-  late Movie currentMovie; // Current movie being questioned about
-  List<dynamic> answerOptions = []; // Options for the user to choose from
-  bool isLoading = true; // Loading state indicator
-  QuestionType? questionType; // Current type of question
+  int currentRound = 1;
+  int score = 0;
+  late Movie currentMovie;
+  List<dynamic> answerOptions = [];
+  bool isLoading = true;
+  QuestionType? questionType;
 
   @override
   void initState() {
     super.initState();
-    fetchAndSetupRound(); // Sets up the game round
+    fetchAndSetupRound();
   }
 
-  // Asynchronously sets up the game round.
+  // Modified fetchAndSetupRound function to check connectivity
   fetchAndSetupRound() async {
-    setState(() =>
-        isLoading = true); // Shows a loading indicator while data is fetched.
+    setState(() => isLoading = true);
+
+    // Check network connectivity
+    var connectivityResult = await Connectivity().checkConnectivity();
+    if (connectivityResult == ConnectivityResult.none) {
+      // No connectivity: Notify the user and save the current state
+      _notifyUserOfConnectivityIssue();
+      _saveGameState();
+      setState(() => isLoading = false);
+      return;
+    }
+
+    // Fetch data as usual if there is connectivity
     MovieApi api = MovieApi();
-    List<Movie> movies =
-        await api.fetchPopularMovies(); // Fetches popular movies.
+    List<Movie> movies = await api.fetchPopularMovies();
     Random random = Random();
-    // Randomly selects a movie to use for this round.
     currentMovie = movies[random.nextInt(movies.length)];
 
-    // Randomly selects a question type for this round.
     List<QuestionType> questionTypes = QuestionType.values;
     questionType = questionTypes[random.nextInt(questionTypes.length)];
 
-    // Generates answer options based on the question type.
     switch (questionType) {
       case QuestionType.Year:
-        // Creates a list of possible release years.
         answerOptions = List.generate(
             4, (_) => currentMovie.releaseYear - random.nextInt(10) + 5);
-        // Ensures the correct answer is in the options.
         answerOptions[random.nextInt(4)] = currentMovie.releaseYear;
         break;
       case QuestionType.Genre:
-        // List of potential genres. In a real app, this list might be fetched from a server or included in a larger dataset.
-        List<String> allGenres = ['Action', 'Drama', 'Comedy', 'Thriller'];
+        List<String> allGenres = [
+          'Action',
+          'Drama',
+          'Comedy',
+          'Thriller'
+        ]; // Example genres
         answerOptions = currentMovie.genres..shuffle();
-        // Fills up the answer options to have 4 choices, ensuring no duplicates.
         while (answerOptions.length < 4) {
           String randomGenre = allGenres[random.nextInt(allGenres.length)];
           if (!answerOptions.contains(randomGenre))
@@ -134,28 +131,49 @@ class _TriviaGameState extends State<TriviaGame> {
         answerOptions.shuffle();
         break;
       case QuestionType.Director:
-        // Prepares a list of potential directors including the correct one.
         answerOptions = [
-          'John Doe', // Placeholder names; in a real app, these might come from a database.
+          'John Doe',
           'Jane Smith',
           'Michael Bay',
           currentMovie.director
-        ];
+        ]; // Example directors
         answerOptions.shuffle();
         break;
       default:
         break;
     }
 
-    setState(() => isLoading =
-        false); // Hides the loading indicator when setup is complete.
+    setState(() => isLoading = false);
   }
 
-  // Processes the user's answer and navigates to the next screen accordingly.
+  // Function to save the current game state
+  _saveGameState() async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setInt('currentRound', currentRound);
+    await prefs.setInt('score', score);
+    // Save other necessary game state data as needed
+  }
+
+// Function to notify the user of connectivity issues
+  _notifyUserOfConnectivityIssue() {
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        title: Text("Connectivity Issue"),
+        content: Text("You're offline. Check your internet connection."),
+        actions: <Widget>[
+          TextButton(
+            child: Text('OK'),
+            onPressed: () => Navigator.of(context).pop(), // Close the dialog
+          ),
+        ],
+      ),
+    );
+  }
+
   void handleAnswer(dynamic selectedAnswer) {
-    bool isCorrect = false; // Indicator if the user's answer is correct.
+    bool isCorrect = false;
     switch (questionType) {
-      // Checks if the selected answer is correct based on the question type.
       case QuestionType.Year:
         isCorrect = selectedAnswer == currentMovie.releaseYear;
         break;
@@ -168,15 +186,12 @@ class _TriviaGameState extends State<TriviaGame> {
       default:
         break;
     }
-    if (isCorrect) score++; // Increments the score if the answer is correct.
-    navigateToNextScreen(
-        isCorrect); // Navigates to the appropriate next screen.
+    if (isCorrect) score++;
+    navigateToNextScreen(isCorrect);
   }
 
-  // Navigates to the next screen depending on the current round and user's answer.
   navigateToNextScreen(bool isCorrect) {
     if (currentRound < 5) {
-      // If not the final round, navigate to the AnswerScreen.
       Navigator.push(
         context,
         MaterialPageRoute(
@@ -185,7 +200,6 @@ class _TriviaGameState extends State<TriviaGame> {
             currentScore: score,
             currentRound: currentRound,
             onNextRound: () {
-              // Pop the AnswerScreen and setup the next round.
               Navigator.of(context).pop();
               setState(() {
                 currentRound++;
@@ -196,7 +210,6 @@ class _TriviaGameState extends State<TriviaGame> {
         ),
       );
     } else {
-      // If it is the final round, save the score and navigate to the GameOverScreen.
       _saveCurrentScore().then((_) {
         Navigator.pushReplacement(
           context,
@@ -206,40 +219,35 @@ class _TriviaGameState extends State<TriviaGame> {
     }
   }
 
-  // Saves the current score to SharedPreferences.
   _saveCurrentScore() async {
     try {
       final prefs = await SharedPreferences.getInstance();
-      // Fetches the username saved in SharedPreferences.
-      String username = prefs.getString('username') ?? 'Player';
+      String username =
+          prefs.getString('username') ?? 'Player'; // Fetch the entered name
 
       List<String> highScores = prefs.getStringList('highScores') ?? [];
       String newScoreEntry = '$username: $score';
 
-      // Update the high score list with the new score.
+      // Update the score for the user if it's higher than the existing one or if there is no existing one
       int existingIndex =
           highScores.indexWhere((entry) => entry.startsWith('$username:'));
       if (existingIndex != -1) {
-        // Replace existing score if one is found.
         highScores[existingIndex] = newScoreEntry;
       } else {
-        // Otherwise, add the new score entry.
         highScores.add(newScoreEntry);
       }
 
-      // Sort the high scores in descending order.
+      // Sort the list by scores in descending order
       highScores.sort((a, b) => int.parse(b.split(":")[1].trim())
           .compareTo(int.parse(a.split(":")[1].trim())));
 
-      // Keep only the top 5 high scores if necessary.
+      // If you want to keep only the top 5 scores
       if (highScores.length > 5) {
         highScores = highScores.sublist(0, 5);
       }
 
-      // Save the updated high scores back to SharedPreferences.
       await prefs.setStringList('highScores', highScores);
     } catch (e) {
-      // Handle errors in saving the score, such as a failed write to SharedPreferences.
       print('Failed to save score: $e');
     }
   }
@@ -247,41 +255,34 @@ class _TriviaGameState extends State<TriviaGame> {
   @override
   Widget build(BuildContext context) {
     if (isLoading) {
-      // Show a loading spinner while the game is being set up.
       return Scaffold(
           appBar: AppBar(title: Text('Movie Trivia')),
           body: Center(child: CircularProgressIndicator()));
     }
 
-    // Main game screen with movie image, question, and answer options.
     return Scaffold(
-      appBar: AppBar(
-          title: Text('Movie Trivia')), // AppBar title for the game screen.
+      appBar: AppBar(title: Text('Movie Trivia')),
       body: Center(
         child: SingleChildScrollView(
-          // Allows the user to scroll if the content does not fit on the screen.
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
               SizedBox(height: 20),
-              // Displays the current movie image.
               Image.network(currentMovie.imageUrl,
                   height: 400,
                   width: double.infinity,
                   fit: BoxFit.contain,
-                  errorBuilder: (context, error, stackTrace) => Icon(
-                      Icons.error)), // Error icon if the image fails to load.
+                  errorBuilder: (context, error, stackTrace) =>
+                      Icon(Icons.error)),
               SizedBox(height: 20),
-              Divider(thickness: 2), // Visual divider for layout separation.
-              // Displays the question based on the current movie and question type.
+              Divider(thickness: 2),
               Text(
                 getQuestionText(),
                 style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
                 textAlign: TextAlign.center,
               ),
               SizedBox(height: 20),
-              // Dynamically creates buttons for each answer option.
               ...answerOptions.map((option) => Padding(
                     padding: const EdgeInsets.symmetric(vertical: 8.0),
                     child: ElevatedButton(
@@ -301,7 +302,6 @@ class _TriviaGameState extends State<TriviaGame> {
     );
   }
 
-  // Generates the question text based on the current movie and question type.
   String getQuestionText() {
     switch (questionType) {
       case QuestionType.Year:
