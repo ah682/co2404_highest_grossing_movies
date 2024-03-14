46eafbbaeb239bdde06df2bfe72bfe433d319d4d
ah682 <96323387+ah682@users.noreply.github.com>
2024-03-14 06:53:08 +0000

    Implement Firebase
    Implement Firebase for Android OS

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-import 'src/models/font_model.dart';
-import 'src/screens/intro_screen.dart';
+import 'src/models/font_model.dart'; // Adjust the import path as necessary
+import 'src/screens/intro_screen.dart'; // Adjust the import path as necessary
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  // Initialize Firebase
+  await Firebase.initializeApp();
+
   final prefs = await SharedPreferences.getInstance();
   final isDarkMode = prefs.getBool('darkMode') ?? false;
 
@@ -19,7 +24,8 @@ void main() async {
           ),
         ),
       ],
-      child: MyApp(),
+      child:
+          MyApp(), // Ensure you have a MyApp widget or replace with your app's main widget
     ),
   );
 }
--- lib/src/screens/enter_name.dart
+++ lib/src/screens/enter_name.dart
@@ -1,32 +1,36 @@
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'trivia_question_screen.dart'; // Ensure the import path matches the file location
+import 'package:cloud_firestore/cloud_firestore.dart';
 
-// EnterNameScreen is a StatefulWidget that prompts users to enter their name before starting the game.
 class EnterNameScreen extends StatefulWidget {
   final int
       score; // Holds the score, passed to this screen from the previous screen.
 
-  // Constructor with required score parameter.
   const EnterNameScreen({Key? key, required this.score}) : super(key: key);
 
   @override
   _EnterNameScreenState createState() => _EnterNameScreenState();
 }
 
-// The state for EnterNameScreen where the logic and UI for user name entry is defined.
 class _EnterNameScreenState extends State<EnterNameScreen> {
-  // TextEditingController is used to read and control the text field input.
   final TextEditingController _nameController = TextEditingController();
 
-  // Method to save the entered name to SharedPreferences and navigate to the TriviaGame screen.
   void _saveNameAndScoreAndRestartGame() async {
-    // Retrieve the SharedPreferences instance.
     final prefs = await SharedPreferences.getInstance();
-    // Save the entered name with the 'username' key.
-    await prefs.setString('username', _nameController.text);
+    await prefs.setString(
+        'username', _nameController.text); // Save the username locally
 
-    // Replace the current route with the TriviaGame route, effectively restarting the game.
+    // Also, send the username to Firestore for cloud storage
+    final FirebaseFirestore firestore = FirebaseFirestore.instance;
+    await firestore.collection('players').add({
+      'name': _nameController.text,
+      // You can include other data to be stored in Firestore here, such as the score
+      'score': widget.score,
+      'timestamp': FieldValue
+          .serverTimestamp(), // Add a server-side timestamp for sorting/ordering
+    });
+
     Navigator.of(context).pushAndRemoveUntil(
       MaterialPageRoute(builder: (context) => TriviaGame()),
       (Route<dynamic> route) => false,
@@ -35,34 +39,26 @@ class _EnterNameScreenState extends State<EnterNameScreen> {
 
   @override
   Widget build(BuildContext context) {
-    // Scaffold provides the structure of the screen.
     return Scaffold(
       appBar: AppBar(
-        title: Text('Enter Your Name'), // Title for the AppBar.
+        title: Text('Enter Your Name'),
       ),
-      // Padding adds space around the child Column.
       body: Padding(
         padding: EdgeInsets.all(16.0),
-        // Column lays out its children vertically and centers them.
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            // TextField allows the user to input text.
             TextField(
-              controller:
-                  _nameController, // Connects the text field to the controller.
+              controller: _nameController,
               decoration: InputDecoration(
-                labelText: 'Name', // Label shown inside the text field.
-                border:
-                    OutlineInputBorder(), // Adds a border around the text field.
+                labelText: 'Name',
+                border: OutlineInputBorder(),
               ),
             ),
-            SizedBox(
-                height: 20), // Space between the text field and the button.
-            // ElevatedButton triggers the save and restart game process.
+            SizedBox(height: 20),
             ElevatedButton(
               onPressed: _saveNameAndScoreAndRestartGame,
-              child: Text('Start Game'), // Text displayed on the button.
+              child: Text('Start Game'),
             ),
           ],
         ),
@@ -72,8 +68,7 @@ class _EnterNameScreenState extends State<EnterNameScreen> {
 
   @override
   void dispose() {
-    _nameController
-        .dispose(); // Disposes the controller when the widget is removed from the widget tree.
+    _nameController.dispose();
     super.dispose();
   }
 }
